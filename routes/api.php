3b5352dc01d4cb9638a33a31256642a30f18b903
Galushka Vladislav <galushka.vp@gmail.com>
2021-08-05 11:29:22 +0700

    [Routes] add routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/registration', [RegistrationController::class, 'registration']);
+Route::middleware('auth:api')->group(function () {
+    Route::resource('/v1/tasks', TaskController::class)->only(['index', 'store']);
 });
